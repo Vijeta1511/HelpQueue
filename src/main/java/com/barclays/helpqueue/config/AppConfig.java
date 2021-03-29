f3e840b6afc7085a8094cc0886f39906e67f63a2
vijetaagrawal <agrawalv71@gmail.com>
2021-03-29 21:22:51 +0100

    HQ-28 #setup skeleton code for react.js to create UI Updated spring config

---
+++ src/main/java/com/barclays/helpqueue/config/AppConfig.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.barclays.helpqueue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author vijetaagrawal
+ *
+ */
+@Configuration
+@Profile({ "test", "prod" })
+public class AppConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*");
+			}
+		};
+		
+	}
+}
+		
+	
---
+++ src/main/java/com/barclays/helpqueue/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.barclays.helpqueue.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author vijetaagrawal
+ *
+ */
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
