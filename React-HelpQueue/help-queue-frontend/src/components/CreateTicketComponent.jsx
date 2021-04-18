3a1e1be13ebaa8486bd93a8540a4ebfaffadb08d
vijetaagrawal <agrawalv71@gmail.com>
2021-04-18 02:01:58 +0100

    HQ-4 HQ-34 #Added create ticket button and its routing to display form

---
+++ React-HelpQueue/help-queue-frontend/src/components/CreateTicketComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CreateTicketComponent extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1> Ticket Form </h1>
+            </div>
+        );
+    }
+}
+
+export default CreateTicketComponent;
